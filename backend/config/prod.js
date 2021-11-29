4b41d069a307544db7ea55ff7fa00a92940d5bde
Eli Granat <91833720+EliGranat@users.noreply.github.com>
2021-11-29 18:59:57 +0200

    Update prod.js

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'dbURL': 'mongodb+srv://eliGranat:@cluster0.fglzw.mongodb.net/my_db?retryWrites=true&w=majority',
+    'dbURL': 'mongodb+srv://eliGranat:<password>@cluster0.fglzw.mongodb.net/my_db?retryWrites=true&w=majority',
 }
